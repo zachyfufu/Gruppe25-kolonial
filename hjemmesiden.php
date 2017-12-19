94d7e6c2bfba1bba5c0902e71a72759bef996749
zachyfufu <zachyfufu@yahoo.com>
2017-12-19 11:24:38 +0100

    lagt til hjemmesiden fil og css fil.

---
+++ hjemmesiden.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <?php include "php/header.php"; ?>
+
+<div id="container">
+    <div id="menyer">
+        <img src="images/menyer.png">
+    </div>
+
+ 
+
+</div>
+
+    <?php include "php/footer.php"; ?>
+
+</body>
+
+
+</html>
